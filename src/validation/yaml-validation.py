10ec571cead7ad7bdadbdd4f91238db2a0d0b181
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2025-12-03 03:42:24 -0200

    Update the yaml validation, it is working but i havent completed the validation of the schema yet

--- src/validation/yaml-validation.py
+++ src/validation/yaml-validation.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field, model_validator, field_validator
 from pathlib import Path
 import logging
 from typing import Union
+import polars as pl 
 
 from strategies import date_estrategia, rename_columns_estrategia, dtype_estrategia
 
@@ -16,7 +17,6 @@ class path_validation(BaseModel):
         path = Path(v)
         nombre = path.name
         terminacion = path.suffix
-        tamaño = path.stat().st_size
         
         if not path.exists(): 
             logger.info(f'El archivo {nombre} no existe')
@@ -25,14 +25,12 @@ class path_validation(BaseModel):
         if terminacion not in ['.csv', '.parquet']:
             logger.info((f'El archivo {nombre} no debe tener una terminacion .csv o .parquet'))
             raise ValueError(f'El archivo {nombre} no debe tener una terminacion .csv o .parquet')
-        
-        if tamaño 
-        
         return path
 
 class cleaning_steps_validation(BaseModel): 
-    data_type: Union[dict, str, date_estrategia, None]
-    rename_columns: Union[dict, str, dtype_estrategia, rename_columns_estrategia, None]
+    data_type: Union[dict, str, None]
+    date_format: date_estrategia
+    rename_columns: Union[dict, str, None]
 
 class validation_data_validation(BaseModel): 
     sample_size: float = Field(..., gt=0, le=1, )
@@ -40,11 +38,25 @@ class validation_data_validation(BaseModel):
 class etl_validation(BaseModel): 
     cleaning_steps: cleaning_steps_validation
     validation_data: validation_data_validation
+
+class validation_yaml(BaseModel): 
+    path: path_validation
+    etl: etl_validation
     
     @model_validator(mode='after')
-    def etl_validator_params(self): 
-        path = self.cleaning_steps.input_path
-        tamaño = path.stat().st_size
-        terminacion = path.suffix
+    def etl_column_type_validation(self): 
+        path= self.path.input_path
+        schema= pl.read_csv(path, n_rows=1000) if path.endswith('.csv') else pl.read_parquet(path, n_rows=1000).schema
         
-        df_lazy = 
+        type_disponible= [tipo.value for tipo in dtype_estrategia]
+        date_type_disponible= [tipo.value for tipo in date_estrategia]
+        
+        for col, tipo in schema.items(): 
+            if col not in schema: 
+                logger.error(f'La columna {col} no se encuentra en el Schema del Frame\n')
+                raise ValueError(f'La columna {col} no se encuentra en el Schema del Frame')
+            elif tipo not in type_disponible:
+                if tipo in date_type_disponible:
+                    continue
+                logger.error(f'El tipo de dato {tipo} no se ecuentra disponible en los tipos de datos\n')
+                raise ValueError((f'El tipo de dato {tipo} no se ecuentra disponible en los tipos de datos'))
