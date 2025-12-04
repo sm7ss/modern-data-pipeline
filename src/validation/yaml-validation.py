80323f0d7e6eb4dcc47e620bb13132242d71f6d3
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2025-12-04 03:22:07 -0200

    Added new validations for columns, dtypes and conversions of dtypes

--- src/validation/yaml-validation.py
+++ src/validation/yaml-validation.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel, Field, model_validator, field_validator
 from pathlib import Path
 import logging
-from typing import Union
+from typing import Union, Dict
 import polars as pl 
 
-from strategies import date_estrategia, rename_columns_estrategia, dtype_estrategia
+from ..strategies.strategies import date_estrategia, rename_columns_estrategia, dtype_estrategia
+from ..etl.dtype_cleaning import DtypeExprList
+from .yaml_classes_type_validation import DictValidation, FormatExistence, RenameColumnsValidation
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger = logging.getLogger(__name__)
@@ -28,9 +30,9 @@ class path_validation(BaseModel):
         return path
 
 class cleaning_steps_validation(BaseModel): 
-    data_type: Union[dict, str, None]
+    data_type: Union[Dict[str, str], None]
     date_format: date_estrategia
-    rename_columns: Union[dict, str, None]
+    rename_columns: Union[str, None]
 
 class validation_data_validation(BaseModel): 
     sample_size: float = Field(..., gt=0, le=1, )
@@ -45,18 +47,55 @@ class validation_yaml(BaseModel):
     
     @model_validator(mode='after')
     def etl_column_type_validation(self): 
+        '''
+            - Valida diccionario si existe 
+            - Accede a la clase DataValidation para validar: 
+                - Columnas existentes 
+                - Tipos de datos disponibles 
+                - Valida las conversiones antes de pasar al ETL 
+        '''
+        path= self.path.input_path
+        df= pl.read_csv(path, n_rows=1000) if path.endswith('.csv') else pl.read_parquet(path, n_rows=1000)
+        schema= df.schema
+        
+        dict_validation= DictValidation(data_type=self.etl.cleaning_steps.data_type)
+        
+        if self.etl.cleaning_steps.data_type: 
+            dict_validation.exist_column(schema=schema)
+            dict_validation.exist_type()
+            dict_validation.dtype_transformer_validation(df=df)
+        return self
+    
+    @model_validator(mode='after')
+    def date_format_validation(self): 
+        '''
+            - Validamos formato en caso de existir 
+            - Si hay un formato pero no hay columnas de fecha actuamos: 
+                - No para el pipeline, pero aparecera en el logging
+        '''
+        path= self.path.input_path
+        df= pl.read_csv(path, n_rows=1000) if path.endswith('.csv') else pl.read_parquet(path, n_rows=1000)
+        schema= df.schema
+        
+        if self.etl.cleaning_steps.date_format: 
+            lista_columnas_fecha= FormatExistence.date_column_exists(df=df, schema=schema, format=self.etl.cleaning_steps.date_format)
+            if not lista_columnas_fecha: 
+                logger.warning(f'No se encontraron columnas númericas, por lo que no habrá conversiones de tipo de fecha para ninguna columna')
+        return self
+    
+    @model_validator(mode='after')
+    def rename_columns_validation(self): 
+        '''
+            - Validamos la columna existente en caso de ser diccionario
+            - Validamos la estrategía en caso de ser string
+        '''
         path= self.path.input_path
         schema= pl.read_csv(path, n_rows=1000) if path.endswith('.csv') else pl.read_parquet(path, n_rows=1000).schema
         
-        type_disponible= [tipo.value for tipo in dtype_estrategia]
-        date_type_disponible= [tipo.value for tipo in date_estrategia]
+        rename_column= self.etl.cleaning_steps.rename_columns
         
-        for col, tipo in schema.items(): 
-            if col not in schema: 
-                logger.error(f'La columna {col} no se encuentra en el Schema del Frame\n')
-                raise ValueError(f'La columna {col} no se encuentra en el Schema del Frame')
-            elif tipo not in type_disponible:
-                if tipo in date_type_disponible:
-                    continue
-                logger.error(f'El tipo de dato {tipo} no se ecuentra disponible en los tipos de datos\n')
-                raise ValueError((f'El tipo de dato {tipo} no se ecuentra disponible en los tipos de datos'))
+        if isinstance(rename_column, dict): 
+            DictValidation(data_type=rename_column).exist_column(schema=schema)
+        else: 
+            RenameColumnsValidation.strategy_rename(strategy=rename_column)
+        return self
