9aa952d2520fdc4244a3cd26998867bc15526f29
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2025-12-17 03:32:14 -0200

    Refactor validation classes and update imports for schema validation.

--- src/validation/config_validation.py
+++ src/validation/config_validation.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel, Field, model_validator, field_validator
 from pathlib import Path
 import logging
-from typing import Union, Dict, Optional
+from typing import Dict, Optional
 import polars as pl 
 
-from ..strategies.strategies import date_estrategia, rename_columns_estrategia, dtype_estrategia
-from ..etl.dtype_cleaning import create_cast_expr
+from ..strategies.strategies import rename_columns_estrategia, dtype_estrategia
+from ..etl.ETL import DataTypeCleaning
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger = logging.getLogger(__name__)
@@ -28,30 +28,52 @@ class path_validation(BaseModel):
             raise ValueError(f'El archivo {nombre} no debe tener una terminacion .csv o .parquet')
         return path
 
-class cleaning_steps_validation(BaseModel): 
-    data_type: Union[Dict[str, str], None]
-    date_format: Optional[date_estrategia]
-    rename_columns: Union[Dict[str, str], rename_columns_estrategia, None]
+class schema_config_validation(BaseModel): 
+    column_naming: Optional[rename_columns_estrategia]
+    date_format: Optional[bool]
+    data_type: Optional[Dict[str, str]]
+    decimal_precision: Optional[int]
+    
+    @field_validator('column_naming')
+    def column_naming_validation(cls, v): 
+        if not v: 
+            return 'lower'
+        else: 
+            return v
+    
+    @field_validator('date_format')
+    def date_format_validation(cls,v): 
+        if not v: 
+            return False
+        else: 
+            return v
     
     @field_validator('data_type')
-    def tipo_validacion(cls, v): 
+    def data_type_validation(cls, v): 
+        if not v: 
+            return v
+        
         tipos= [v_tipo.value[0] for v_tipo in dtype_estrategia]
         for valor in v.values(): 
             if valor not in tipos: 
                 logger.error(f'El tipo de valor {valor} no está disponible en estrategias\n')
                 raise ValueError(f'El tipo de valor {valor} no está disponible en estrategias')
         return v
+    
+    @field_validator('decimal_precision')
+    def decimal_precision_validation(cls, v): 
+        if not v: 
+            return 2 
+        else: 
+            return v
 
 class validation_data_validation(BaseModel): 
-    sample_size: float = Field(..., gt=0, le=1, )
-
-class etl_validation(BaseModel): 
-    cleaning_steps: cleaning_steps_validation
-    validation_data: validation_data_validation
+    sample_size: float = Field(..., gt=0, le=1)
 
 class validation_yaml(BaseModel): 
     path: path_validation
-    etl: etl_validation
+    schema_config: schema_config_validation
+    validation_data: validation_data_validation
     
     @model_validator(mode='after')
     def column_type_validation(self): 
@@ -60,7 +82,7 @@ class validation_yaml(BaseModel):
         schema= frame.schema
         
         #Validar columnas y conversion de tipo de datos para data_type 
-        data_type= self.etl.cleaning_steps.data_type
+        data_type= self.schema_config.data_type
         if data_type:
             for col in data_type: 
                 if col not in schema: 
@@ -69,17 +91,8 @@ class validation_yaml(BaseModel):
             
             for col, tipo in data_type.items(): 
                 try:
-                    frame.with_columns(create_cast_expr(col=col, dtype=tipo))
+                    frame.with_columns(DataTypeCleaning().create_cast_expr(col=col, dtype=tipo))
                 except Exception: 
                     logger.error(f'Ocurrio un error al querer tranformar la columna {col} a el tipo de dato {tipo}\n')
                     raise ValueError(f'currio un error al querer tranformar la columna {col} a el tipo de dato {tipo}')
-        
-        #Validar columnas para rename_columns
-        rename_columns= self.etl.cleaning_steps.rename_columns
-        if rename_columns:
-            if isinstance(rename_columns, dict): 
-                for col in rename_columns: 
-                    if col not in schema: 
-                        logger.error(f'La columna {col} no se encuentra en el DataFrame del archivo\n')
-                        raise ValueError(f'La columna {col} no se encuentra en el DataFrame del archivo\n')
         return self
