38bd33c865e3f836c047af2954e52c4d12c5ef89
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2025-12-21 03:06:24 -0200

    Modified the file to spare responsabilities for the future streaming engine

--- src/etl/PipelineETL.py
+++ src/etl/PipelineETL.py
@@ -1,51 +1,12 @@
 import polars as pl 
-from typing import Dict, Any, Union
+from typing import Dict, Any
 import logging 
-import psutil
 from pydantic import BaseModel
-from pathlib import Path
 
-from ETL import PipelineETL
+from .ETL import PipelineETL
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger= logging.getLogger(__name__)
-
-class ChunkFileProcessor: 
-    def __init__(self, path: Path, file_size: Union[int, float]):
-        self.path= path
-        
-        self.size_file= file_size
-        self.available_ram= psutil.virtual_memory().available
-        self.num_cpu= psutil.cpu_count(logical=False)
-    
-    def estimate_max_chunk_radio(self) -> float: 
-        ratio= self.size_file / self.available_ram
-        
-        if ratio > 5 : 
-            return 0.15 
-        elif ratio > 2: 
-            return 0.25
-        elif ratio > 0.5: 
-            return 0.4
-        else: 
-            return 0.7
-    
-    def estimate_min_chunks(self) -> int: 
-        return max(5, self.num_cpu*2)
-    
-    def calculate_optimal_chunk_size(self, safe_margin: float) -> int: 
-        ram_for_use= self.available_ram * safe_margin
-        max_chunk_ratio= self.estimate_max_chunk_radio()
-        min_chunks= self.estimate_min_chunks()
-        
-        ram_limit= int(ram_for_use * max_chunk_ratio)
-        partition_limit= self.size_file // min_chunks
-        
-        chunk_size= min(ram_limit, partition_limit)
-        chunk_size= max(chunk_size, 1024**2)
-        chunk_size= min(chunk_size, self.size_file)
-        
-        return chunk_size
 
 class ETL: 
     def __init__(self, model: BaseModel, file_overhead: Dict[str, Any]):
@@ -56,7 +17,7 @@ class ETL:
         self.file_size= file_overhead['tamaño_archivo']
         self.os_margin= file_overhead['os_margin']
         
-        self.chunk_size= ChunkFileProcessor(path=self.archivo, file_size=self.file_size)
+        #self.chunk_size= ChunkFileProcessor(path=self.archivo, file_size=self.file_size)
     
     def _load_eager_frame(self) -> pl.DataFrame: 
         if self.archivo.suffix == '.csv': 
@@ -71,44 +32,8 @@ class ETL:
             return pl.scan_parquet(self.archivo)
     
     def _run_streaming_handler(self) -> None:
-        chunk_size= self.chunk_size.calculate_optimal_chunk_size(safe_margin=self.os_margin)
-        
-        if self.archivo.suffix == '.csv': 
-            primer_chunk= pl.read_csv(
-                self.archivo, 
-                n_rows=chunk_size
-            )
-        else: 
-            #Cambiar estrategia a n_groups
-            logger.warning('Streaming por Parquet no disponible aun')
-        
-        #Meter el etl de primera transformacion 
-        #Escribirlo em archivo para el sample 
-        
-        schema= primer_chunk.schema
-        
-        skip_rows= chunk_size
-        while True: 
-            try: 
-                if self.archivo.suffix == '.csv': 
-                    siguiente_chunk= pl.read_csv(
-                        self.archivo, 
-                        skip_rows=skip_rows,
-                        n_rows=chunk_size,
-                        schema_overrides=schema
-                    )
-                else: 
-                    logger.warning('Streaming por Parquet no disponible aun')
-                    break 
-                
-                if siguiente_chunk.height == 0: 
-                    break
-                
-                #Meter el etl de primera transformacion 
-                
-            except Exception as e:
-                logger.warning(f'Fin del archivo o error: {e}')
-                break
+        # Dividimos la logica de csv y parquet 
+        pass
     
     def orquestador_pipeline(self) -> None: 
         if self.decision == 'eager': 
@@ -118,5 +43,7 @@ class ETL:
             frame= self._load_lazy_frame() 
             etl_frame= PipelineETL(Frame=frame, model=self.model)
         else: 
-            self._run_streaming_handler() 
+            self._run_streaming_handler() # Aun no soportado 
+            logger.warning('Metodo aun no soportado para streaming')
+            raise 
 
