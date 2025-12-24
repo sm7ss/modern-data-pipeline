b73b4ac169dce3c3a0ba65f2c802e403a27158fa
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2025-12-24 02:41:23 -0200

    Now csv is soported and can be used

--- src/etl/Streaming.py
+++ src/etl/Streaming.py
@@ -2,9 +2,11 @@ import logging
 import polars as pl 
 import pyarrow as pa
 import pyarrow.parquet as pp
-import psutil
-from typing import Dict, Any
+from typing import Dict, Any, Union
 from pathlib import Path
+from pydantic import BaseModel
+
+from .ETL import PipelineETL
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger= logging.getLogger(__name__)
@@ -32,17 +34,69 @@ class StreamingCSVHandler:
         
         batch_rows= int(total_rows*batch_float)
         
-        batch_rows= max(batch_rows)
+        batch_rows= max(batch_rows, 10000)
         batch_rows= min(batch_rows, total_rows)
         return batch_rows
     
-    def run_streaming(self) -> None: 
-        pass
+    def run_streaming(self, ETL: PipelineETL, model: BaseModel) -> None: 
+        #Hacer otro engine aquí en caso de que las rows sean demasiadas para procesar en eager o lazy mode
+        row_size= self.csv_batch_size_row()
+        
+        first_chunk= pl.read_csv(self.archivo, n_rows=row_size)
+        etl= ETL(Frame= first_chunk, model=model)
+        frame= etl.etl()
+        
+        logger.info(f'Columnas {frame.height} procesadas exitosamente')
+        
+        #Experimental
+        if frame.height==row_size: 
+            return frame
+        
+        schema= frame.schema
+        frame.write_csv('pandera_report.csv')
+        
+        skip_chunk=row_size
+        while True: 
+            try: 
+                next_chunk= pl.read_csv(
+                    self.archivo, 
+                    skip_rows=skip_chunk, 
+                    n_rows=row_size, 
+                    schema_overrides=schema
+                )
+                
+                if next_chunk.height==0: 
+                    logger.warning('Archivo sin más filas a procesar')
+                    return next_chunk
+                
+                frame= etl.etl()
+                
+                logger.info(f'Columnas {frame.height} procesadas exitosamente.\n{skip_chunk} saltadas')
+                skip_chunk+=row_size
+            except Exception as e: 
+                logger.warning(f'Fin del archivo u ocurrio un error: \n{e}')
+                break
 
 class StreamingParquetHanlder: 
     def __init__(self, archivo: Path, file_overhead: Dict[str, Any]):
         self.archivo= archivo
-        self.file_overhead= file_overhead
+        self.file_overhead= file_overhead['parquet_metadata']
     
-    def read_parquet_streaming(self) -> float: 
+    def calculate_row_size(self) -> int: 
         pass
+    
+    def bytes_row_group(self, target_size: Union[int, float]) -> int: 
+        rows=0
+        bytes_used= 0
+        
+        for rg in range(self.row_group.num_row_groups): 
+            rg_meta= self.row_group.row_group(rg)
+            rg_size=sum(
+                rg_meta.column(col).total_uncompressed_size
+                for col in range(self.row_group.num_columns)
+            )
+            return rg_size
+
+class StreamingCleaningData: 
+    #orquestador 
+    pass
