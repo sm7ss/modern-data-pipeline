f3bac55add928c56c5091dadc1d67b7d937348e2
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2025-12-31 02:39:52 -0200

    Refactor EngineDecision class to improve initialization and file overhead handling. Added logging for better traceability and updated methods for processing file overhead

--- src/etl/EngineDecision.py
+++ src/etl/EngineDecision.py
@@ -1,19 +1,31 @@
 import polars as pl 
 from typing import Dict, Any, Optional
 import logging 
-from pydantic import BaseModel
+from pathlib import Path
 
 from .ETL import PipelineETL
 from .Streaming import PipelineStreaming
+from ..validation.ReadYamlValidation import ReadSchemaValidation
+from ..memory_optimizer.PathDecisionMaker import PipelineEstimatedSizeFiles
+from ..validation.PanderaSchema import PanderaSchema
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger= logging.getLogger(__name__)
 
 class EngineDecision: 
-    def __init__(self, model: BaseModel, file_overhead: Dict[str, Any]):
-        self.model= model
+    def __init__(self, archivo: str, os_margin: float=0.3, n_rows_sample: int=1000):
+        self.os_margin= os_margin
+        self.n_rows_sample= n_rows_sample
+        
+        self.model= ReadSchemaValidation(archivo=archivo).read_file()
         self.archivo = self.model.path.input_path
-        self.file_overhead= file_overhead
+        self.file_overhead= self.file_overhead(archivo=self.archivo)[0]
+    
+    def file_overhead(self, archivo: str) -> Dict[str, Any]: 
+        archivo= Path(archivo)
+        diccionario= PipelineEstimatedSizeFiles(archivo=archivo, os_margin=self.os_margin, n_rows_sample=self.n_rows_sample).estimated_size_file()
+        logger.info(f'Se obtuvo el file_overhead para el archivo {self.archivo.name}')
+        return diccionario, archivo
     
     def _load_eager_frame(self) -> pl.DataFrame: 
         if self.archivo.suffix == '.csv': 
@@ -39,10 +51,28 @@ class EngineDecision:
         
         if decision == 'eager': 
             frame= self._load_eager_frame()
-            PipelineETL(Frame=frame, model=self.model)
+            logger.info(f'Se obtuvo el frame exitosamente con la decision {decision}')
+            
+            frame= PipelineETL(Frame=frame, model=self.model).etl()
+            frame.write_parquet('pandera_report.parquet')
+            
+            logger.info(f'Se tranformo el frame exitosamente para el archivo {self.archivo.name}')
+            diccionario, archivo= self.file_overhead(archivo='pandera_report.parquet')
+            PanderaSchema(model=self.model, archivo=archivo, file_overhead=diccionario).validation_schema()
         elif decision == 'lazy': 
             frame= self._load_lazy_frame() 
-            PipelineETL(Frame=frame, model=self.model)
+            logger.info(f'Se obtuvo el frame exitosamente con la decision {decision}')
+            
+            porcentaje= self.model.validation_data.sample_size
+            total_filas_slice= int(self.file_overhead_model['total_de_filas']*porcentaje)
+            
+            frame= PipelineETL(Frame=frame, model=self.model).etl()
+            frame.slice(0, total_filas_slice).collect(engine='streaming').write_parquet('pandera_report.parquet')
+            
+            logger.info(f'Se tranformo el frame exitosamente para el archivo {self.archivo.name}')
+            diccionario, archivo = self.file_overhead(archivo='pandera_report.parquet')
+            PanderaSchema(model=self.model, archivo=archivo, file_overhead=diccionario).validation_schema()    
         else: 
-            return self._run_streaming_handler() 
+            diccionario= self._run_streaming_handler() 
+            return diccionario
 
