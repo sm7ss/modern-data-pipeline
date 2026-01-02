45cd7396fae27380bcd2114f0bb69ad286afa506
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-01-02 21:21:46 -0200

    Validation was added for the OS configuration and also for the database configuration. Keep in mind: In os_configuration, for n_rows_sample, the maximum number of rows that can be set is 1000 and the minimum is 100. If you set more or less, it will automatically adjust to 100 or 1000 according to the limit. In database, for table_name, if the table exists, if_table_exists will be considered; if it doesn't exist, a new table will be created. For if_table_exists, you can set replace, append, or fail, and if you don't set anything, the default will be fail. The risk of not setting anything is that the execution may fail when inserting data into the database after the ETL, so caution is advised

--- src/validation/ConfigValidation.py
+++ src/validation/ConfigValidation.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, field_validator, model_validator, Field
 import polars as pl 
 import logging
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict, Optional, Literal
 
 from ..strategies.Strategies import rename_columns_estrategia, dtype_estrategia
 from ..etl.ETL import DataTypeCleaning
@@ -67,6 +67,66 @@ class schema_config_validation(BaseModel):
         else: 
             return v
 
+class os_configuration_validation(BaseModel): 
+    os_margin: float= Field(ge=0.1, le=0.5)
+    n_rows_sample: int
+    
+    @field_validator('n_rows_sample')
+    def n_rows_rample_validation(cls, v): 
+        if v > 1000: 
+            logger.warning(f'Se modifico el valor de "n_rows_sample" de {v} a 1000 puesto que el limite maximo para "n_rows_sample" son 1000 filas')
+            v= 1000
+        elif v < 100: 
+            logger.warning(f'Se modifico el valor de "n_rows_sample" de {v} a 100 puesto que el limite minimo para "n_rows_sample" son 100 filas')
+            v=100
+        else: 
+            return v
+        return v
+
+class database_validation(BaseModel): 
+    table_name: str
+    if_table_exists: Optional[Literal['append', 'replace', 'fail']]
+    
+    @field_validator('if_table_exists')
+    def if_table_exists_validation(cls, v): 
+        if not v: 
+            v= 'fail'
+            logger.warning('Como no se puso un valor pre-definido para si la tabla existe, se asigno "fail" y en caso de existir la tabla se dará un error. El costo de la operación esta en que se ejecutará el pipeline y fallara al tratar de meterse los datos procesados a la base de datos.')
+        return v
+    
+    @field_validator('table_name')
+    def table_name_validation(cls, v): 
+        if len(v)==0: 
+            logger.warning(f'El nuevo_nombre de la tabla es el nombre del archivo; puesto que no se puso ningun nombre en la configuracion')
+            return 'new_table'
+        
+        len_v= len(v)
+        v= v.strip()
+        len_v_later= len(v)
+        if not(len_v == len_v_later): 
+            logger.warning('Se quitaron los espacios de inicio y fin de la palabra')
+        
+        if '-' in v: 
+            v= v.replace('-', '_')
+            logger.warning('Se remplazaron los caracteres - por _')
+        
+        if ' ' in v: 
+            v= v.replace(' ', '')
+            logger.warning('Se quitaron los espacios en blanco entre las palabras')
+        
+        for l in v: 
+            try:
+                if int(l): 
+                    logger.warning(f'"{v}" no puede tener numeros en incio de la palabra')
+                    nuevo_nombre= v.split(l)[1]
+                    if len(nuevo_nombre)==0: 
+                        logger.warning(f'El nuevo_nombre de la tabla es el nombre del archivo; puesto que el nombre de la tabla eran unicamente numeros')
+                        return 'new_table'
+                    v= nuevo_nombre
+            except Exception: 
+                logger.info(f'Se obtuvo exitosamente el nuevo nombre de la tabla.\nEl nuevo nombre de la tabla es: "{v}"')
+                return v
+
 class validation_data_validation(BaseModel): 
     sample_size: float = Field(gt=0.0, le=1.0)
 
@@ -74,6 +134,8 @@ class validation_yaml(BaseModel):
     path: path_validation
     schema_config: schema_config_validation
     validation_data: validation_data_validation
+    os_configuration: os_configuration_validation
+    database: database_validation
     
     @model_validator(mode='after')
     def column_type_validation(self): 
@@ -96,3 +158,12 @@ class validation_yaml(BaseModel):
                     logger.error(f'Ocurrio un error al querer tranformar la columna {col} a el tipo de dato {tipo}\n')
                     raise ValueError(f'currio un error al querer tranformar la columna {col} a el tipo de dato {tipo}')
         return self
+    
+    @model_validator(mode='after')
+    def table_name(self): 
+        archivo= self.path.input_path.stem
+        
+        if self.database.table_name=='new_table': 
+            self.database.table_name= archivo
+        return self
+
