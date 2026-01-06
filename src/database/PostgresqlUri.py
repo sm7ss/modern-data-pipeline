fdc5c4823a738c3abda9d849d93327220849052c
Arbolitos Mi Casita <ns.7m.sw@gmail.com>
2026-01-06 03:56:02 -0200

    Batching was added to insert data persistently into the database. This is to prevent the data pipeline from crashing due to too many conversions or files that are too large

--- src/database/PostgresqlUri.py
+++ src/database/PostgresqlUri.py
@@ -1,14 +1,12 @@
 from dotenv import load_dotenv
 import os
 import polars as pl
-from pathlib import Path
-from typing import Dict, Any
+import gc
+from typing import Dict, Any, Callable, Optional
 import logging
 
 import psycopg2
 import io 
-
-from ..etl.Streaming import StreamingCSVHandler
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(asctime)s-%(message)s')
 logger= logging.getLogger(__name__)
@@ -28,8 +26,8 @@ class PostgresDatabase:
         return uri
     
     @classmethod
-    def optimal_batch_size(cls, archivo: Path, file_overhead: Dict[str, Any]) -> int: 
-        risk_factor= StreamingCSVHandler(archivo=archivo, file_overhead=file_overhead).estimate_batch_size()
+    def optimal_batch_size(cls, StreamingCSVHandler: Callable, file_overhead: Dict[str, Any]) -> int: 
+        risk_factor= StreamingCSVHandler
         bytes_total_file= file_overhead.get('memoria_total_estimada', 'archivo_descomprimido')
         filas_totales= file_overhead['total_de_filas']
         safety_memory= file_overhead['safety_memory']
@@ -44,13 +42,42 @@ class PostgresDatabase:
         return batch_size
     
     @classmethod
-    def insert_data_to_database(cls, frame: pl.LazyFrame, archivo: str,  table_name: str, file_overhead: Dict[str, Any]) -> None: 
-        optimal_batch_size= cls.optimal_batch_size(archivo=archivo, file_overhead=file_overhead)
+    def optimal_batch_size_streaming(cls, StreamingCSVHandler: Callable, frame: pl.LazyFrame, n_rows_size: int, file_overhead: Dict[str, Any]) -> int: 
+        risk_factor= StreamingCSVHandler
+        bytes_total_frame= frame.estimated_size()
+        safety_memory= file_overhead['safety_memory']
+        
+        bytes_per_row= bytes_total_frame/n_rows_size
+        max_batch= ((risk_factor*safety_memory)/bytes_per_row)
+        
+        batch_size= min(max_batch, 250000)
+        batch_size= max(batch_size, 10000)
+        batch_size= min(batch_size, n_rows_size)
+        logger.info(f'El batch_size oprimto es de {batch_size}')
+        return batch_size
+    
+    @classmethod
+    def insert_data_to_database(cls, StreamingCSVHandler: Callable, frame: pl.LazyFrame,  table_name: str, file_overhead: Dict[str, Any], if_table_exists: str, n_rows: Optional[int]=None) -> None: 
+        decision= file_overhead['decision']
+        if decision in ['eager', 'lazy']: 
+            filas_totales= file_overhead['total_de_filas']
+            optimal_batch_size= cls.optimal_batch_size(StreamingCSVHandler=StreamingCSVHandler, file_overhead=file_overhead)
+        else: 
+            filas_totales= n_rows
+            #esto consume demasiado, no apto realmente para streaming, a menos que los bathces de procesameinto en streaming sean demasiado grandes
+            optimal_batch_size= cls.optimal_batch_size_streaming(StreamingCSVHandler=StreamingCSVHandler, frame=frame, n_rows_size=filas_totales, file_overhead=file_overhead)
+        
         uri= cls.uri_database()
-        filas_totales= file_overhead['total_de_filas']
+        conn= None
         
         try: 
             conn= psycopg2.connect(uri)
+            
+            frame.slice(0,0).collect(engine='streaming').write_database(
+                table_name=table_name, 
+                connection=uri, 
+                if_table_exists=if_table_exists
+            )
             
             for offset in range(0, filas_totales, optimal_batch_size): 
                 df= frame.slice(offset, optimal_batch_size).collect(engine='streaming')
@@ -62,10 +89,14 @@ class PostgresDatabase:
                 with conn.cursor() as cur: 
                     cur.copy_expert(f'COPY {table_name} FROM STDIN WITH CSV HEADER', csv_buff)
                 logger.info(f'Batch {offset//optimal_batch_size+1} insertado ({len(df)} filas)')
+                
+                del df
+                gc.collect()
+            conn.commit()
         except Exception as e: 
             logger.error(f'Ocurrio un error al querer insertar los datos a la tabla {table_name}.\n{e}')
             conn.rollback()
             raise 
         finally: 
-            conn.commit()
-            conn.close()
+            if conn: 
+                conn.close()
